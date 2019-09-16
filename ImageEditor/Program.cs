86cb5fd6bc0773ad0ed9955467310e16559f25a0
sara <saraa379@yahoo.com>
2019-09-16 13:59:50 +0200

    started

---
+++ ImageEditor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ImageEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create image.
+            //Image newImage = Image.FromFile(args[0]);
+            Console.WriteLine("Image Editor is starting");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No argument is passed");
+            }
+            else
+            {
+                string argument = args[0];
+                Console.WriteLine("Argument is passed");
+            }
+        }
+    }
+}
